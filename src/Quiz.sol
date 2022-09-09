9ecae35a143f5020f36c9c8a3474668d6b38fb72
beomjoon <beomjoon.kim@dream-academy.io>
2022-09-10 01:54:01 +0900

    paractice02

--- src/Quiz.sol
+++ src/Quiz.sol
@@ -10,38 +10,82 @@ contract Quiz{
       uint max_bet;
    }
     
-    mapping(address => uint256)[] public bets;
+    mapping(uint => mapping(address => uint256)) public bets;
+    // mapping(address => uint256)[] public bets;
+    address owner;
     uint public vault_balance;
+    Quiz_item[] quizs;
+    mapping(address => uint256) bal;
 
     constructor () {
+        owner = msg.sender;
         Quiz_item memory q;
         q.id = 1;
         q.question = "1+1=?";
         q.answer = "2";
         q.min_bet = 1 ether;
         q.max_bet = 2 ether;
+        Quiz_item memory dum;
+        addQuiz(q);    
         addQuiz(q);
     }
 
     function addQuiz(Quiz_item memory q) public {
+        
+        require(owner == msg.sender);
+        quizs.push(q);
     }
 
     function getAnswer(uint quizId) public view returns (string memory){
+        return quizs[quizId].answer;
     }
 
     function getQuiz(uint quizId) public view returns (Quiz_item memory) {
+        Quiz_item memory q=quizs[quizId];
+        q.answer = "";
+        return q;
     }
 
     function getQuizNum() public view returns (uint){
+        return quizs.length-1;
     }
     
     function betToPlay(uint quizId) public payable {
+        uint256 ethValue = msg.value;
+        Quiz_item memory q = quizs[quizId];
+        uint idx = quizId-1;
+        require(q.max_bet >= ethValue);
+        require(q.min_bet <= ethValue);
+
+
+        bets[idx][msg.sender]+=msg.value;
+
     }
 
+  
+
+
     function solveQuiz(uint quizId, string memory ans) public returns (bool) {
+        Quiz_item memory q = quizs[quizId];
+        uint256 idx = quizId-1;
+        if(keccak256(abi.encodePacked(q.answer)) != keccak256(abi.encodePacked(ans))){
+            vault_balance+=bets[idx][msg.sender];
+            bets[idx][msg.sender] = 0;
+            return false;
+        }
+        bal[msg.sender] = q.min_bet*2;
+
+        return true;
     }
 
     function claim() public {
+        uint256 balance = bal[msg.sender];
+        bal[msg.sender]=0;
+        (bool isSuccess,)=msg.sender.call{value:balance}("");
+        assert(isSuccess);
     }
 
+    receive() external payable {
+        vault_balance+=msg.value;
+    }
 }
